61b7060d432a21a8e3807f3587d7f8d9dc3b5fca
Quentin Renard <contact@asticode.com>
2017-05-28 03:22:20 +0200

    Added astitools

--- astimodaler/astimodaler.js
+++ astimodaler/astimodaler.js
@@ -2,32 +2,27 @@ if (typeof asticode === "undefined") {
     var asticode = {};
 }
 asticode.modaler = {
-    close: function() {
+    hide: function() {
         document.getElementById("astimodaler").style.display = "none";
     },
     init: function() {
-        document.body.innerHTML = `
-        <div class="astimodaler" id="astimodaler">
-            <div class="astimodaler-background" id="astimodaler-background"></div>
+        document.body.innerHTML = `<div class="astimodaler" id="astimodaler">
+            <div class="astimodaler-background"></div>
             <div class="astimodaler-table">
                 <div class="astimodaler-wrapper">
                     <div class="astimodaler-body">
-                        <i class="fa fa-close" id="astimodaler-close"></i>
-                        <div class="astimodaler-content" id="astimodaler-content"></div>
+                        <i class="fa fa-close astimodaler-close" onclick="asticode.modaler.hide()"></i>
+                        <div id="astimodaler-content"></div>
                     </div>
                 </div>
             </div>
-        </div>
-        ` + document.body.innerHTML;
-        document.getElementById("astimodaler-close").onclick = function() {
-            alert("bite");
-            asticode.modaler.close();
-        }
+        </div>` + document.body.innerHTML;
     },
-    open: function(html) {
-        var content = document.getElementById("astimodaler-content");
-        content.innerHTML = '';
-        content.appendChild(html);
+    setContent: function(content) {
+        document.getElementById("astimodaler-content").innerHTML = '';
+        document.getElementById("astimodaler-content").appendChild(content);
+    },
+    show: function() {
         document.getElementById("astimodaler").style.display = "block";
     }
 };
