d753f5725d7838068d14dc39412289cedfeb419b
Quentin Renard <contact@asticode.com>
2019-08-04 15:01:32 +0200

    Updated loader

--- astiloader/astiloader.js
+++ astiloader/astiloader.js
@@ -2,6 +2,7 @@ if (typeof asticode === "undefined") {
     var asticode = {};
 }
 asticode.loader = {
+    scriptDir: document.currentScript.src.match(/.*\//),
     hide: function() {
         document.getElementById("astiloader").style.display = "none";
     },
@@ -9,7 +10,7 @@ asticode.loader = {
         document.body.innerHTML = `
         <div class="astiloader" id="astiloader">
             <div class="astiloader-background"></div>
-            <div class="astiloader-table"><div class="astiloader-content"><i class="fa fa-spinner fa-spin fa-3x fa-fw"></i></div></div>
+            <div class="astiloader-table"><div class="astiloader-content"><img src="` + asticode.loader.scriptDir + `/loader.png"/></div></div>
         </div>
         ` + document.body.innerHTML
     },
